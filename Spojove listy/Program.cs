e8182df3ba4cc1791989d6ce368ebd7df6f17a59
BagrNaDruhou <sejhar@gchd.cz>
2024-10-16 15:54:44 +0200

    Updated

--- Spojove listy/Program.cs
+++ Spojove listy/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace Spojove_listy;
+﻿using System.Runtime.Remoting.Messaging;
+
+namespace Spojove_listy;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Node uzlik = new Node(8);
     }
+}
+class Node
+{
+    public Node(int value)
+    {
+        Value = value;
+    }
+    public int Value { get; }
+    public Node Next {  get; set; }
+}
+class LinkedList
+{
+    public Node Head { get; set; }
+
+    public void Add(int value)
+    {
+        if (Head == null)
+            Head = new Node(value);
+        else
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+    }
+    public bool Find(int value)
+    {
+        if (Head == null)
+            return false;
+        Node node = Head;
+        while (node.Next != null)
+        {
+            if(node.Value == value)
+                return true;
+            node = node.Next;
+        }
+    }   return false;
 }
