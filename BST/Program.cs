6632bab5a11bcbe99edd4eaa011526dbf876fd07
BagrNaDruhou <sejhar@gchd.cz>
2025-01-22 15:56:27 +0100

    hodina progress

---
+++ BST/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<Person> node = new Node<Person>();
+            Node<int> id = new Node<int>();
+        }
+    }
+
+    class Node<T>
+    {
+        public int Key { get; }
+        public T Value { get; }
+    }
+}
