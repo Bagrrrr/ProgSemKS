676c6c0c36e243bafe5feb0718ce0b3371953a39
Bagrrrr <sejhar@gchd.cz>
2025-04-26 14:01:29 +0200

    Prefix a Postfix
    Funguje skvěle, v nouzi možno upravit na +20b

--- Connect 4/Program.cs
+++ Connect 4/Program.cs
@@ -11,16 +11,8 @@ namespace Connect_4
     {
         static void Main(string[] args)
         {
-            int[,] board = new int[6, 7]{
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 1, 2, 1, 0 },
-                { 2, 2, 2, 2, 2, 1, 0 }
-            };
-            int[] position = { 5, 3 };
-            ConnectFour hra1 = new ConnectFour();
+            ConnectFour hra1 = new ConnectFour(4,7,6,4);
+            hra1.Play();
             Console.ReadLine();
         }
     }
@@ -29,12 +21,113 @@ namespace Connect_4
         public ConnectFour(int winNum, int columnNum, int rowNum, int playerAmount) 
         {
             this.winNum = winNum;
-            board = new int[columnNum, rowNum];
+            board = new int[rowNum, columnNum];
             hraci = new Hrac[playerAmount];
+            hraciNum = playerAmount;
+            List<string> symbols = new List<string> { "X", "O", "A", "B", "C", "D", "E", "F", "G", "H" };
+
+            for (int i = 0; i < playerAmount; i++)
+            {
+                hraci[i] = new Hrac
+                {
+                    Jmeno = $"Player {i + 1}",
+                    Symbol = symbols[i % symbols.Count],
+                    Cislo = (i + 1).ToString()
+                };
+            }
         }
-        int winNum; // datová položka
+        int winNum;
         int[,] board;
         Hrac[] hraci;
+        int hraciNum;
+
+        public void Play()
+        {
+            int currentPlayer = 0;
+            bool konecHry = false;
+            while (!konecHry)
+            {
+                PrintBoard(board);
+                Console.WriteLine($"{hraci[currentPlayer].Jmeno} na tahu");
+                int column = GetColumn(board.GetLength(1), board);
+                int row = PlacePiece(board, column, currentPlayer);
+                int [] hrana = new int[] { row, column };
+                if (Check(board, winNum, currentPlayer + 1, hrana))
+                {
+                    konecHry = true;
+                    Console.Clear();
+                    PrintBoard(board);
+                    Console.WriteLine($"{hraci[currentPlayer].Jmeno} vyhrál!");
+                }
+                else if (IsFull(board))
+                {
+                    konecHry = true;
+                    Console.Clear();
+                    PrintBoard(board);
+                    Console.WriteLine("Remíza!");
+                }
+                currentPlayer = (currentPlayer + 1) % hraciNum;
+            }
+        }
+        public static int PlacePiece(int[,] board , int column, int player)
+        {
+            for (int i = board.GetLength(0) - 1; i >= 0; i--)
+            {
+                if (board[i, column] == 0)
+                {
+                    board[i, column] = player + 1;
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void PrintBoard(int[,] dvaDpole)
+        {
+            Console.Clear();
+            for (int i = 0; i < dvaDpole.GetLength(0); i++)
+            {
+                for (int j = 0; j < dvaDpole.GetLength(1); j++)
+                {
+                    if (dvaDpole[i, j] == 0)
+                    {
+                        Console.Write(dvaDpole[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(hraci[dvaDpole[i, j] - 1].Symbol + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int GetColumn(int columnAmount, int[,] board)
+        {
+            int column;
+            while (true)
+            {
+                Console.Write("Zadej sloupec: ");
+                if (int.TryParse(Console.ReadLine(), out column) && column > 0 && column <= columnAmount)
+                {
+                    if (board[0, column - 1] == 0)
+                        return column - 1;
+                }
+                Console.WriteLine("Neplatný vstup");
+            }
+        }
+        public static bool IsFull(int[,] dvaDpole)
+        {
+            for (int i = 0; i < dvaDpole.GetLength(0); i++)
+            {
+                for (int j = 0; j < dvaDpole.GetLength(1); j++)
+                {
+                    if (dvaDpole[i, j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
         public static bool CheckRow(int[,] dvaDpole, int pocetNaVyhru, int hrac, int[] hranaPozice)
         {
             int CheckedRow = hranaPozice[0];
@@ -148,6 +241,7 @@ namespace Connect_4
     {
         public string Jmeno;
         public string Symbol;
+        public string Cislo;
     }
 
 }
---
+++ Prefix a Postfix/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prefix_a_Postfix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string priklad = Console.ReadLine();
+            PrefixOrPostfix prefixOrPostfix = new PrefixOrPostfix();    
+            Console.WriteLine(prefixOrPostfix.Vysledek(priklad));
+        }
+    }
+
+    public class PrefixOrPostfix
+    {
+
+        public float? Vysledek(string priklad)
+        {
+            bool typ = PrefixCheck(priklad);
+            if (typ == true)
+            {
+                return Prefix(priklad);
+            }
+            else
+            {
+                return Postfix(priklad);
+            }
+        }
+        float? Prefix(string priklad)
+        {
+            string[] kousky = priklad.Split(' ');
+            Stack<string> stack = new Stack<string>();
+            int x = 0;
+            int y = 0;
+            while (1>0)
+            {
+                if (stack.Count > 2)
+                {
+                    string top1 = stack.Pop();
+                    string top2 = stack.Pop();
+
+                    if (float.TryParse(top2, out float float1) && float.TryParse(top1, out float float2))
+                    {
+                        string operace = stack.Pop();
+                        float vysledek = 0;
+                        switch (operace)
+                        {
+                            case "+":
+                                vysledek = float1 + float2;
+                                break;
+                            case "-":
+                                vysledek = float1 - float2;
+                                break;
+                            case "*":
+                                vysledek = float1 * float2;
+                                break;
+                            case "/":
+                                if (float2 != 0)
+                                {
+                                    vysledek = float1 / float2;
+                                    break;
+                                }
+                                else
+                                    Console.WriteLine("Dělení nulou");
+                                    return null;
+                            default:
+                                Console.WriteLine("Neznámá operace");
+                                return null;
+                        }
+                        stack.Push(vysledek.ToString());
+                    }
+                    else
+                    {
+                        stack.Push(top2);
+                        stack.Push(top1);
+                    }
+                }
+                if (stack.Count == 1)
+                {
+                    string vysledek = stack.Pop();
+                    if (float.TryParse(vysledek, out float vysl))
+                    {
+                        return vysl;
+                    }
+                    else
+                    {
+                        stack.Push(vysledek);
+                    }
+                }
+                if (y % 2 == 0)
+                {
+                    try
+                    {
+                        stack.Push(kousky[x]);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Nedostatek");
+                        return null;
+                    }
+                    x += 1;
+                }
+                y += 1;
+            }
+        }
+
+
+
+        float? Postfix(string priklad)
+        {
+            string[] kousky = priklad.Split(' ');
+            Stack<float> stack = new Stack<float>();
+            for (int i = 0; i < kousky.Length; i++)
+            {
+                string momentalnik = kousky[i];
+                if (float.TryParse(momentalnik, out float cislo))
+                {
+                    stack.Push(cislo);
+                }
+                else
+                {
+                    if (stack.Count < 2)
+                    {
+                        Console.WriteLine("Nedostatek");
+                        return null;
+                    }
+                    float top1 = stack.Pop();
+                    float top2 = stack.Pop();
+                    float vysledek = 0;
+                    switch (momentalnik)
+                    {
+                        case "+":
+                            vysledek = top2 + top1;
+                            break;
+                        case "-":
+                            vysledek = top2 - top1;
+                            break;
+                        case "*":
+                            vysledek = top2 * top1;
+                            break;
+                        case "/":
+                            if (top1 != 0)
+                            {
+                                vysledek = top2 / top1;
+                                break;
+                            }
+                            else
+                                Console.WriteLine("Dělení nulou");
+                            return null;
+                        default:
+                            Console.WriteLine("Neznámá operace");
+                            return null;
+                    }
+                    stack.Push(vysledek);
+
+
+                }
+            }
+            if (stack.Count == 1)
+            {
+                return stack.Pop();
+            }
+            else
+            {
+                Console.WriteLine("Nedostatek");
+                return null;
+            }
+
+
+
+        }
+        bool PrefixCheck(string prefix)
+        {
+            if (Char.IsDigit(prefix[0]) == true)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
+
