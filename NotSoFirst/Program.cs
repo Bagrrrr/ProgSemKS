070f4a681cb1479375fcb7733f424aecc98d230a
Bagrrrr <sejhar@gchd.cz>
2025-09-14 13:40:53 +0200

    Novy rok a stale naprosto tapu

---
+++ NotSoFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotSoFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Film
+    {
+        public string Nazev;
+        public string JmenoRezisera;
+        public string PrijmeniRezisera;
+        public int RokVzniku;
+    }
+}
